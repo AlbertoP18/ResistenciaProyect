266a916cddc419f86421494d8659f2ff58f913e6
AlbertoP18 <alberto.perez9@misena.edu.co>
2023-08-04 21:17:11 -0500

    subida de tarea de programacion de bandas de resistencia GUI

---
+++ src/resistencia/Resistencia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resistencia;
+
+/**
+ *
+ * @author THAMAR DUSSAN
+ */
+public class Resistencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Identificador_Resistencias pantalla = new Identificador_Resistencias();
+        pantalla.setVisible (true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+    
+}
